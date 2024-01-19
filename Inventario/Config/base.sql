c211f6f135dba0f9c416ddecfa369abf2c7ea551
Dereck2002 <jaramilloderek13@gmail.com>
2024-01-19 17:06:58 -0500

    ......

--- Inventario/Config/base.sql
+++ Inventario/Config/base.sql
@@ -2,114 +2,132 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:8889
--- Tiempo de generación: 10-01-2024 a las 23:49:12
--- Versión del servidor: 5.7.39
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-01-2024 a las 23:06:01
+-- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de datos: `Inventario_Sexto`
+-- Base de datos: `examendos`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Productos`
+-- Estructura de tabla para la tabla `inventario`
 --
 
-CREATE TABLE `Productos` (
-  `ProductoId` int(11) NOT NULL,
-  `Nombre` text NOT NULL,
-  `Precio` decimal(8,2) NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `FechaIngreso` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `inventario` (
+  `ID_producto` int(11) NOT NULL,
+  `ID_proveedor` int(11) DEFAULT NULL,
+  `Nombre_producto` varchar(255) NOT NULL,
+  `Cantidad` int(11) DEFAULT NULL,
+  `Precio_unitario` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`ID_producto`, `ID_proveedor`, `Nombre_producto`, `Cantidad`, `Precio_unitario`) VALUES
+(1, 1, 'Mangos', 2, '1.00'),
+(2, 2, 'Cosina', 1, '400.00');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Proveedores`
+-- Estructura de tabla para la tabla `proveedores`
 --
 
-CREATE TABLE `Proveedores` (
-  `ProveedorId` int(11) NOT NULL,
-  `Nombres` text NOT NULL,
-  `Telefono` text NOT NULL,
-  `Correo` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `proveedores` (
+  `ID_proveedor` int(11) NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Producto_suministrado` varchar(255) NOT NULL,
+  `Fecha_inicio_contrato` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`ID_proveedor`, `Nombre`, `Producto_suministrado`, `Fecha_inicio_contrato`) VALUES
+(1, 'Mario pineda', 'Frutas', '2024-01-19'),
+(2, 'Estiven Toala', 'Electrodomesticos', '2024-01-31');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Stocks`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `Stocks` (
-  `StockId` int(11) NOT NULL,
-  `ProductoId` int(11) NOT NULL,
-  `ProveedorId` int(11) NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `Precio_Venta` decimal(8,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `usuario` (
+  `UsuarioId` int(11) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contrasenia` varchar(30) NOT NULL,
+  `Rol` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`UsuarioId`, `correo`, `contrasenia`, `Rol`) VALUES
+(1, 'jaramilloderek13@gmail.com', '123', 'Admin');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `Productos`
+-- Indices de la tabla `inventario`
 --
-ALTER TABLE `Productos`
-  ADD PRIMARY KEY (`ProductoId`);
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`ID_producto`),
+  ADD KEY `ID_proveedor` (`ID_proveedor`);
 
 --
--- Indices de la tabla `Proveedores`
+-- Indices de la tabla `proveedores`
 --
-ALTER TABLE `Proveedores`
-  ADD PRIMARY KEY (`ProveedorId`);
-
---
--- Indices de la tabla `Stocks`
---
-ALTER TABLE `Stocks`
-  ADD PRIMARY KEY (`StockId`),
-  ADD KEY `Proveedor_Stock` (`ProveedorId`),
-  ADD KEY `ProductoId` (`ProductoId`);
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`ID_proveedor`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `Productos`
+-- AUTO_INCREMENT de la tabla `inventario`
 --
-ALTER TABLE `Productos`
-  MODIFY `ProductoId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `inventario`
+  MODIFY `ID_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `Proveedores`
+-- AUTO_INCREMENT de la tabla `proveedores`
 --
-ALTER TABLE `Proveedores`
-  MODIFY `ProveedorId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `Stocks`
---
-ALTER TABLE `Stocks`
-  MODIFY `StockId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `proveedores`
+  MODIFY `ID_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `Stocks`
+-- Filtros para la tabla `inventario`
 --
-ALTER TABLE `Stocks`
-  ADD CONSTRAINT `ProductoId` FOREIGN KEY (`ProductoId`) REFERENCES `Productos` (`ProductoId`),
-  ADD CONSTRAINT `Proveedor_Stock` FOREIGN KEY (`ProveedorId`) REFERENCES `Proveedores` (`ProveedorId`);
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`ID_proveedor`) REFERENCES `proveedores` (`ID_proveedor`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
